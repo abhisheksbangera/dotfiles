ee3d3d33203243c1935728fc73d0e6ad61e94787
Abhishek Bangera <abhishekbangera10@gmail.com>
2024-12-03 00:02:44 +0530

    theme and telescope updated

--- .config/nvim/lua/abhishek/plugins/colorscheme.lua
+++ .config/nvim/lua/abhishek/plugins/colorscheme.lua
@@ -1,40 +1,28 @@
 return {
-    {
-      "folke/tokyonight.nvim",
-      priority = 1000, -- make sure to load this before all the other start plugins
-      config = function()
-        local bg = "#011628"
-        local bg_dark = "#011423"
-        local bg_highlight = "#143652"
-        local bg_search = "#0A64AC"
-        local bg_visual = "#275378"
-        local fg = "#CBE0F0"
-        local fg_dark = "#B4D0E9"
-        local fg_gutter = "#627E97"
-        local border = "#547998"
-  
-        require("tokyonight").setup({
-          style = "night",
-          on_colors = function(colors)
-            colors.bg = bg
-            colors.bg_dark = bg_dark
-            colors.bg_float = bg_dark
-            colors.bg_highlight = bg_highlight
-            colors.bg_popup = bg_dark
-            colors.bg_search = bg_search
-            colors.bg_sidebar = bg_dark
-            colors.bg_statusline = bg_dark
-            colors.bg_visual = bg_visual
-            colors.border = border
-            colors.fg = fg
-            colors.fg_dark = fg_dark
-            colors.fg_float = fg
-            colors.fg_gutter = fg_gutter
-            colors.fg_sidebar = fg_dark
-          end,
-        })
-        -- load the colorscheme here
-        vim.cmd([[colorscheme tokyonight]])
-      end,
-    },
+  'shaunsingh/nord.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- Example config in lua
+    vim.g.nord_contrast = true
+    vim.g.nord_borders = false
+    vim.g.nord_disable_background = true
+    vim.g.nord_italic = false
+    vim.g.nord_uniform_diff_background = true
+    vim.g.nord_bold = false
+
+    -- Load the colorscheme
+    require('nord').set()
+
+    -- Toggle background transparency
+    local bg_transparent = true
+
+    local toggle_transparency = function()
+      bg_transparent = not bg_transparent
+      vim.g.nord_disable_background = bg_transparent
+      vim.cmd [[colorscheme nord]]
+    end
+
+    vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
+  end,
 }
